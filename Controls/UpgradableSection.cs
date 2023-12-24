e2fb83eb3a380c67ab0caecd2c6bfc993f8bd758
Nicholas Martin <nickmartin1ee7@gmail.com>
2023-12-24 02:23:18 -0500

    Ramp up progress speed to 30s

--- Controls/UpgradableSection.cs
+++ Controls/UpgradableSection.cs
@@ -5,148 +5,150 @@ using Godot;
 
 public partial class UpgradableSection : Node2D
 {
-	private Button _upgradeButton;
-	private ProgressBar _seedProgressBar;
-	private Label _itemNameLabel;
-	private Label _itemRateLabel;
-	private Label _seedCostLabel;
-	private WorldScene _world;
-	private Seed _seed;
-	private int _multiplier = 1;
-	private Task _progressJob;
+    // 100ms = 10 iterations in 1 second
+    // 10ms = 100 iterations in 1 second
+    // 1ms = 1000 iterations in 1 second
+    private const int ProgressCycleDelay = 33;
 
-	public Seed SeedType
-	{
-		get => _seed;
+    private Button _upgradeButton;
+    private ProgressBar _seedProgressBar;
+    private Label _itemNameLabel;
+    private Label _itemRateLabel;
+    private Label _seedCostLabel;
+    private WorldScene _world;
+    private Seed _seed;
+    private int _multiplier = 1;
+    private Task _progressJob;
 
-		set
-		{
-			if (_seed == value)
-			{
-				return;
-			}
+    public Seed SeedType
+    {
+        get => _seed;
 
-			_seed = value;
-			NotifyStateChanged();
-		}
-	}
-	public int Multiplier
-	{
-		get => _multiplier;
-		set
-		{
-			if (_multiplier == value)
-			{
-				return;
-			}
+        set
+        {
+            if (_seed == value)
+            {
+                return;
+            }
 
-			_multiplier = value;
-			NotifyStateChanged();
-		}
-	}
-	public int Rate => (int)SeedType * Multiplier;
+            _seed = value;
+            NotifyStateChanged();
+        }
+    }
+    public int Multiplier
+    {
+        get => _multiplier;
+        set
+        {
+            if (_multiplier == value)
+            {
+                return;
+            }
 
-	public const string ResourcePath = "res://Controls/upgradable_section.tscn";
+            _multiplier = value;
+            NotifyStateChanged();
+        }
+    }
+    public int Rate => (int)SeedType * Multiplier;
 
-	public EventHandler<long> PointCreated { get; set; }
-	public int UpgradeCost =>
-		(int)SeedType * (int)(SeedCosts.Multiplier / 10d) * Multiplier;
+    public const string ResourcePath = "res://Controls/upgradable_section.tscn";
 
-	// Called when the node enters the scene tree for the first time.
+    public EventHandler<long> PointCreated { get; set; }
+    public int UpgradeCost =>
+        (int)SeedType * (int)(SeedCosts.Multiplier / 10d) * Multiplier;
 
-	public override void _Ready()
-	{
-		_upgradeButton = GetNode<PanelContainer>("UpgradePanel").GetNode<Button>("UpgradeButton");
-		_upgradeButton.Pressed += Upgrade;
+    // Called when the node enters the scene tree for the first time.
 
-		_seedProgressBar = GetNode<ProgressBar>("SeedProgressBar");
-		_itemNameLabel = GetNode<Label>("ItemNameLabel");
-		_itemRateLabel = GetNode<Label>("ItemRateLabel");
-		_seedCostLabel = GetNode<Panel>("SeedCostPanel").GetNode<Label>("SeedCostLabel");
+    public override void _Ready()
+    {
+        _upgradeButton = GetNode<PanelContainer>("UpgradePanel").GetNode<Button>("UpgradeButton");
+        _upgradeButton.Pressed += Upgrade;
 
-		_world = GetParent().GetParent().GetParent().GetParent().GetNode<WorldScene>("WorldScene");
+        _seedProgressBar = GetNode<ProgressBar>("SeedProgressBar");
+        _itemNameLabel = GetNode<Label>("ItemNameLabel");
+        _itemRateLabel = GetNode<Label>("ItemRateLabel");
+        _seedCostLabel = GetNode<Panel>("SeedCostPanel").GetNode<Label>("SeedCostLabel");
 
-		_progressJob ??= Task.Run(ProgressCycle);
+        _world = GetParent().GetParent().GetParent().GetParent().GetNode<WorldScene>("WorldScene");
 
-		NotifyStateChanged();
-	}
+        _progressJob ??= Task.Run(ProgressCycle);
 
-	private async Task ProgressCycle()
-	{
-		GD.Print($"Progress bar min value: {_seedProgressBar.MinValue}, Max Value: {_seedProgressBar.MaxValue}");
+        NotifyStateChanged();
+    }
 
-		while (true)
-		{
-			var progress = _seedProgressBar.Value;
-			// Ensure the progress value is within the valid range
-			var newProgress = Mathf.Clamp(progress + (double)Multiplier / 10,
-				_seedProgressBar.MinValue,
-				_seedProgressBar.MaxValue);
+    private async Task ProgressCycle()
+    {
+        GD.Print($"Progress bar min value: {_seedProgressBar.MinValue}, Max Value: {_seedProgressBar.MaxValue}");
 
-			CallThreadSafe(nameof(SetProgressBarValue), newProgress);
+        while (true)
+        {
+            var progress = _seedProgressBar.Value;
+            // Ensure the progress value is within the valid range
+            var newProgress = Mathf.Clamp(progress + (double)Multiplier / 10,
+                _seedProgressBar.MinValue,
+                _seedProgressBar.MaxValue);
 
-			// 100ms = 10 iterations in 1 second
-			// 10ms = 100 iterations in 1 second
-			// 1ms = 1000 iterations in 1 second
-			await Task.Delay(10);
-		}
-	}
+            CallThreadSafe(nameof(SetProgressBarValue), newProgress);
 
-	private void SetProgressBarValue(double value) =>
-		_seedProgressBar.Value = value;
+            await Task.Delay(ProgressCycleDelay);
+        }
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (_seedProgressBar.Value == 100d)
-		{
-			_seedProgressBar.Value = 0d;
-			PointCreated?.Invoke(this, (int)SeedType);
-		}
-	}
+    private void SetProgressBarValue(double value) =>
+        _seedProgressBar.Value = value;
 
-	private void BuyMultiplier()
-	{
-		_world.Points -= UpgradeCost;
-		Multiplier++;
-		NotifyStateChanged();
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (_seedProgressBar.Value == 100d)
+        {
+            _seedProgressBar.Value = 0d;
+            PointCreated?.Invoke(this, (int)SeedType);
+        }
+    }
 
-	private void Upgrade()
-	{
-		if (!CanPurchaseMultiplier())
-		{
-			GD.Print($"Cannot afford to purchase upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
-			return;
-		}
+    private void BuyMultiplier()
+    {
+        _world.Points -= UpgradeCost;
+        Multiplier++;
+        NotifyStateChanged();
+    }
 
-		GD.Print($"Purchasing upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
-		BuyMultiplier();
-	}
+    private void Upgrade()
+    {
+        if (!CanPurchaseMultiplier())
+        {
+            GD.Print($"Cannot afford to purchase upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
+            return;
+        }
 
-	private bool CanPurchaseMultiplier() =>
-		_world.Points >= UpgradeCost;
+        GD.Print($"Purchasing upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
+        BuyMultiplier();
+    }
 
-	private void NotifyStateChanged()
-	{
-		if (_itemNameLabel is not null)
-		{
-			_itemNameLabel.Text = $"{SeedType}";
-		}
+    private bool CanPurchaseMultiplier() =>
+        _world.Points >= UpgradeCost;
 
-		if (_itemRateLabel is not null)
-		{
-			_itemRateLabel.Text = $"{(int)SeedType}";
-		}
+    private void NotifyStateChanged()
+    {
+        if (_itemNameLabel is not null)
+        {
+            _itemNameLabel.Text = $"{SeedType}";
+        }
 
-		if (_upgradeButton is not null)
-		{
-			_upgradeButton.Text = $"x{Multiplier}";
-		}
+        if (_itemRateLabel is not null)
+        {
+            _itemRateLabel.Text = $"{(int)SeedType}";
+        }
 
-		if (_seedCostLabel is not null)
-		{
-			_seedCostLabel.Text = $"{UpgradeCost}";
-		}
-	}
+        if (_upgradeButton is not null)
+        {
+            _upgradeButton.Text = $"x{Multiplier}";
+        }
+
+        if (_seedCostLabel is not null)
+        {
+            _seedCostLabel.Text = $"{UpgradeCost}";
+        }
+    }
 }
