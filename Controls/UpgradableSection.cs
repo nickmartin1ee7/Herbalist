ce281bf52a7bb9dd51b92fea87fa0e1b6b6847a6
Nicholas Martin <nickmartin1ee7@gmail.com>
2023-12-24 20:32:59 -0500

    add large number formatting

--- Controls/UpgradableSection.cs
+++ Controls/UpgradableSection.cs
@@ -5,171 +5,171 @@ using Godot;
 
 public partial class UpgradableSection : Node2D
 {
-	// 100ms = 10 iterations in 1 second
-	// 10ms = 100 iterations in 1 second
-	// 1ms = 1000 iterations in 1 second
-	private const int CycleDelay = 5;
+    // 100ms = 10 iterations in 1 second
+    // 10ms = 100 iterations in 1 second
+    // 1ms = 1000 iterations in 1 second
+    private const int CycleDelay = 5;
 
-	private Button _upgradeButton;
-	private Sprite2D[] _upgradeIcons;
-	private ProgressBar _seedProgressBar;
-	private Label _itemNameLabel;
-	private Label _itemRateLabel;
-	private Label _seedCostLabel;
-	private WorldScene _world;
-	private Seed _seed;
-	private int _multiplier = 1;
-	private Task _progressJob;
+    private Button _upgradeButton;
+    private Sprite2D[] _upgradeIcons;
+    private ProgressBar _seedProgressBar;
+    private Label _itemNameLabel;
+    private Label _itemRateLabel;
+    private Label _seedCostLabel;
+    private WorldScene _world;
+    private Seed _seed;
+    private int _multiplier = 1;
+    private Task _progressJob;
 
-	public Seed SeedType
-	{
-		get => _seed;
+    public Seed SeedType
+    {
+        get => _seed;
 
-		set
-		{
-			if (_seed == value)
-			{
-				return;
-			}
+        set
+        {
+            if (_seed == value)
+            {
+                return;
+            }
 
-			_seed = value;
-			NotifyStateChanged();
-		}
-	}
-	public int Multiplier
-	{
-		get => _multiplier;
-		set
-		{
-			if (_multiplier == value)
-			{
-				return;
-			}
+            _seed = value;
+            NotifyStateChanged();
+        }
+    }
+    public int Multiplier
+    {
+        get => _multiplier;
+        set
+        {
+            if (_multiplier == value)
+            {
+                return;
+            }
 
-			_multiplier = value;
-			NotifyStateChanged();
-		}
-	}
-	public int Rate => (int)SeedType * Multiplier;
+            _multiplier = value;
+            NotifyStateChanged();
+        }
+    }
+    public int Rate => (int)SeedType * Multiplier;
 
-	public const string ResourcePath = "res://Controls/upgradable_section.tscn";
+    public const string ResourcePath = "res://Controls/upgradable_section.tscn";
 
-	public EventHandler<long> PointCreated { get; set; }
-	public int UpgradeCost =>
-		(int)SeedType * (int)(SeedCosts.Multiplier / 10d) * Multiplier;
+    public EventHandler<long> PointCreated { get; set; }
+    public int UpgradeCost =>
+        (int)SeedType * (int)(SeedCosts.Multiplier / 10d) * Multiplier;
 
-	// Called when the node enters the scene tree for the first time.
+    // Called when the node enters the scene tree for the first time.
 
-	public override void _Ready()
-	{
-		var upgradePanel = GetNode<PanelContainer>("UpgradePanel");
-		_upgradeButton = upgradePanel.GetNode<Button>("UpgradeButton");
-		_upgradeButton.Pressed += Upgrade;
+    public override void _Ready()
+    {
+        var upgradePanel = GetNode<PanelContainer>("UpgradePanel");
+        _upgradeButton = upgradePanel.GetNode<Button>("UpgradeButton");
+        _upgradeButton.Pressed += Upgrade;
 
-		_upgradeIcons = new[]
-		{
-			upgradePanel.GetNode<Sprite2D>("UpgradeIcon"),
-			upgradePanel.GetNode<Sprite2D>("UpgradeIcon2")
-		};
+        _upgradeIcons = new[]
+        {
+            upgradePanel.GetNode<Sprite2D>("UpgradeIcon"),
+            upgradePanel.GetNode<Sprite2D>("UpgradeIcon2")
+        };
 
-		SetUpgradeIconVisibility(visible: false);
+        SetUpgradeIconVisibility(visible: false);
 
-		_seedProgressBar = GetNode<ProgressBar>("SeedProgressBar");
-		_itemNameLabel = GetNode<Label>("ItemNameLabel");
-		_itemRateLabel = GetNode<Label>("ItemRateLabel");
-		_seedCostLabel = GetNode<Panel>("SeedCostPanel").GetNode<Label>("SeedCostLabel");
+        _seedProgressBar = GetNode<ProgressBar>("SeedProgressBar");
+        _itemNameLabel = GetNode<Label>("ItemNameLabel");
+        _itemRateLabel = GetNode<Label>("ItemRateLabel");
+        _seedCostLabel = GetNode<Panel>("SeedCostPanel").GetNode<Label>("SeedCostLabel");
 
-		_world = GetParent().GetParent().GetParent().GetParent().GetNode<WorldScene>("WorldScene");
+        _world = GetParent().GetParent().GetParent().GetParent().GetNode<WorldScene>("WorldScene");
 
-		_progressJob ??= Task.Run(ProgressCycle);
+        _progressJob ??= Task.Run(ProgressCycle);
 
-		NotifyStateChanged();
-	}
+        NotifyStateChanged();
+    }
 
-	private void SetUpgradeIconVisibility(bool visible)
-	{
-		foreach (var upgradeIcon in _upgradeIcons)
-		{
-			upgradeIcon.Visible = visible;
+    private void SetUpgradeIconVisibility(bool visible)
+    {
+        foreach (var upgradeIcon in _upgradeIcons)
+        {
+            upgradeIcon.Visible = visible;
 
-		}
-	}
+        }
+    }
 
-	private async Task ProgressCycle()
-	{
-		GD.Print($"Progress bar min value: {_seedProgressBar.MinValue}, Max Value: {_seedProgressBar.MaxValue}");
+    private async Task ProgressCycle()
+    {
+        GD.Print($"Progress bar min value: {_seedProgressBar.MinValue}, Max Value: {_seedProgressBar.MaxValue}");
 
-		while (true)
-		{
-			var progress = _seedProgressBar.Value;
-			// Ensure the progress value is within the valid range
-			var newProgress = Mathf.Clamp(progress + (double)Multiplier / 10,
-				_seedProgressBar.MinValue,
-				_seedProgressBar.MaxValue);
+        while (true)
+        {
+            var progress = _seedProgressBar.Value;
+            // Ensure the progress value is within the valid range
+            var newProgress = Mathf.Clamp(progress + (double)Multiplier / 10,
+                _seedProgressBar.MinValue,
+                _seedProgressBar.MaxValue);
 
-			CallThreadSafe(nameof(SetProgressBarValue), newProgress);
+            CallThreadSafe(nameof(SetProgressBarValue), newProgress);
 
-			await Task.Delay(CycleDelay);
-		}
-	}
+            await Task.Delay(CycleDelay);
+        }
+    }
 
-	private void SetProgressBarValue(double value) =>
-		_seedProgressBar.Value = value;
+    private void SetProgressBarValue(double value) =>
+        _seedProgressBar.Value = value;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (_seedProgressBar.Value == 100d)
-		{
-			_seedProgressBar.Value = 0d;
-			PointCreated?.Invoke(this, (int)SeedType);
-		}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (_seedProgressBar.Value == 100d)
+        {
+            _seedProgressBar.Value = 0d;
+            PointCreated?.Invoke(this, (int)SeedType);
+        }
 
-		SetUpgradeIconVisibility(CanPurchaseMultiplier());
-	}
+        SetUpgradeIconVisibility(CanPurchaseMultiplier());
+    }
 
-	private void BuyMultiplier()
-	{
-		_world.Points -= UpgradeCost;
-		Multiplier++;
-		NotifyStateChanged();
-	}
+    private void BuyMultiplier()
+    {
+        _world.Points -= UpgradeCost;
+        Multiplier++;
+        NotifyStateChanged();
+    }
 
-	private void Upgrade()
-	{
-		if (!CanPurchaseMultiplier())
-		{
-			GD.Print($"Cannot afford to purchase upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
-			return;
-		}
+    private void Upgrade()
+    {
+        if (!CanPurchaseMultiplier())
+        {
+            GD.Print($"Cannot afford to purchase upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
+            return;
+        }
 
-		GD.Print($"Purchasing upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
-		BuyMultiplier();
-	}
+        GD.Print($"Purchasing upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
+        BuyMultiplier();
+    }
 
-	private bool CanPurchaseMultiplier() =>
-		_world.Points >= UpgradeCost;
+    private bool CanPurchaseMultiplier() =>
+        _world.Points >= UpgradeCost;
 
-	private void NotifyStateChanged()
-	{
-		if (_itemNameLabel is not null)
-		{
-			_itemNameLabel.Text = $"{SeedType}";
-		}
+    private void NotifyStateChanged()
+    {
+        if (_itemNameLabel is not null)
+        {
+            _itemNameLabel.Text = $"{SeedType}";
+        }
 
-		if (_itemRateLabel is not null)
-		{
-			_itemRateLabel.Text = $"{(int)SeedType:N0}";
-		}
+        if (_itemRateLabel is not null)
+        {
+            _itemRateLabel.Text = $"{((long)SeedType).FormatLargeNumber()}";
+        }
 
-		if (_upgradeButton is not null)
-		{
-			_upgradeButton.Text = $"x{Multiplier:N0}";
-		}
+        if (_upgradeButton is not null)
+        {
+            _upgradeButton.Text = $"x{((long)Multiplier).FormatLargeNumber()}";
+        }
 
-		if (_seedCostLabel is not null)
-		{
-			_seedCostLabel.Text = $"{UpgradeCost:N0}";
-		}
-	}
+        if (_seedCostLabel is not null)
+        {
+            _seedCostLabel.Text = $"{((long)UpgradeCost).FormatLargeNumber()}";
+        }
+    }
 }
---
+++ ScriptModels/NumberExtensions.cs
@@ -0,0 +1,51 @@
+﻿public static class NumberExtensions
+{
+    public static string FormatLargeNumber(this long number)
+    {
+        const long Septillion = 1_000_000_000_000_000_000;
+        const long Sextillion = 1_000_000_000_000_000;
+        const long Quintillion = 1_000_000_000_000;
+        const long Quadrillion = 1_000_000_000;
+        const long Trillion = 1_000_000_000;
+        const long Billion = 1_000_000_000;
+        const long Million = 1_000_000;
+        const long Thousand = 1_000;
+
+        if (number >= Septillion)
+        {
+            return $"{(double)number / Septillion:F2} Sept";
+        }
+        else if (number >= Sextillion)
+        {
+            return $"{(double)number / Sextillion:F2} Sext";
+        }
+        else if (number >= Quintillion)
+        {
+            return $"{(double)number / Quintillion:F2} Quint";
+        }
+        else if (number >= Quadrillion)
+        {
+            return $"{(double)number / Quadrillion:F2} Quadr";
+        }
+        else if (number >= Trillion)
+        {
+            return $"{(double)number / Trillion:F2} Tril";
+        }
+        else if (number >= Billion)
+        {
+            return $"{(double)number / Billion:F2} Bil";
+        }
+        else if (number >= Million)
+        {
+            return $"{(double)number / Million:F2} Mil";
+        }
+        else if (number >= Thousand)
+        {
+            return $"{(double)number / Thousand:F2} K";
+        }
+        else
+        {
+            return number.ToString("N0");
+        }
+    }
+}
