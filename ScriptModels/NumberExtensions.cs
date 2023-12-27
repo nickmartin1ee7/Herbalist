7df21392735f61a24edd494d8a2d6ec9ee3cb70d
Nicholas Martin <nickmartin1ee7@gmail.com>
2023-12-27 17:08:44 -0500

    correct number humanized format

--- ScriptModels/NumberExtensions.cs
+++ ScriptModels/NumberExtensions.cs
@@ -2,24 +2,14 @@
 {
     public static string FormatLargeNumber(this long number)
     {
-        const long Septillion = 1_000_000_000_000_000_000;
-        const long Sextillion = 1_000_000_000_000_000;
-        const long Quintillion = 1_000_000_000_000;
-        const long Quadrillion = 1_000_000_000;
-        const long Trillion = 1_000_000_000;
+        const long Quintillion = 1_000_000_000_000_000_000;
+        const long Quadrillion = 1_000_000_000_000_000;
+        const long Trillion = 1_000_000_000_000;
         const long Billion = 1_000_000_000;
         const long Million = 1_000_000;
         const long Thousand = 1_000;
 
-        if (number >= Septillion)
-        {
-            return $"{(double)number / Septillion:F2} Sept";
-        }
-        else if (number >= Sextillion)
-        {
-            return $"{(double)number / Sextillion:F2} Sext";
-        }
-        else if (number >= Quintillion)
+        if (number >= Quintillion)
         {
             return $"{(double)number / Quintillion:F2} Quint";
         }
