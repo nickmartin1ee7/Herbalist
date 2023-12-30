144a5d539ac26ac03f7d974ee01308b40033acfb
Nicholas Martin <nickmartin1ee7@gmail.com>
2023-12-30 00:32:53 -0500

    WIP working and snapping swipeable control

--- Controls/SwipeableControl.cs
+++ Controls/SwipeableControl.cs
@@ -2,31 +2,88 @@ using Godot;
 
 public partial class SwipeableControl : Control
 {
-	private bool _dragging;
+    private enum SnapState
+    {
+        None = 0,
+        SnappingToTop,
+        SnappingToBottom,
+    }
 
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventMouseButton mouseEvent)
-		{
-			if (mouseEvent.ButtonIndex == MouseButton.Left)
-			{
-				if (mouseEvent.Pressed)
-				{
-					_dragging = GetRect().HasPoint(mouseEvent.Position);
-				}
-				else
-				{
-					_dragging = false;
-				}
-			}
-		}
+    private const float MaxY = 384f;
 
-		if (@event is InputEventMouseMotion mouseMotionEvent)
-		{
-			if (_dragging)
-			{
-				Position += mouseMotionEvent.Relative;
-			}
-		}
-	}
+    private bool _dragging;
+    private SnapState _snapState;
+    private Vector2 _initialPos;
+
+    public override void _Ready()
+    {
+        _initialPos = Position;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseEvent
+            && mouseEvent.ButtonIndex == MouseButton.Left)
+        {
+            if (mouseEvent.Pressed)
+            {
+                _dragging = GetRect().HasPoint(mouseEvent.Position);
+            }
+            else if (_dragging)
+            {
+                // Snap to top
+                if (_snapState == SnapState.SnappingToTop)
+                {
+                    GD.Print($"Snap to toping... Position: {Position.Y} -> {MaxY}");
+                    Position = new Vector2(
+                        Position.X,
+                        MaxY);
+                }
+                // Snap to bottom
+                else if (_snapState == SnapState.SnappingToBottom)
+                {
+                    GD.Print($"Snaping to bottom... Position: {Position.Y} -> {_initialPos.Y}");
+                    Position = new Vector2(
+                        Position.X,
+                        _initialPos.Y);
+                }
+
+                _snapState = SnapState.None;
+                _dragging = false;
+            }
+        }
+
+        if (@event is InputEventMouseMotion mouseMotionEvent
+            && _dragging)
+        {
+            const float DeadZone = 0.0f;
+
+            var yChange = Mathf.Round(mouseMotionEvent.Relative.Y);
+            var newYPos = Position.Y + yChange;
+            var lowEnough = newYPos > MaxY;
+            var highEnough = newYPos < _initialPos.Y;
+
+            _snapState = yChange > DeadZone // Going down
+                ? SnapState.SnappingToBottom
+                : SnapState.None;
+
+            _snapState = yChange < -DeadZone // Going up
+                ? SnapState.SnappingToTop
+                : _snapState;
+
+            // Move if within bounds
+            if (lowEnough && highEnough)
+            {
+                GD.Print($"Moving: lowEnough = {lowEnough}. highEnough = {highEnough}. Position: {Position.Y} + {yChange} = {newYPos}. Snap State: {_snapState} _initialPos.Y = {_initialPos.Y}. MaxY = {MaxY}.");
+
+                Position = new Vector2(
+                    Position.X,
+                    newYPos);
+            }
+            else
+            {
+                GD.Print($"Cannot move: lowEnough = {lowEnough}. highEnough = {highEnough}. Position = {Position.Y} + {yChange} => {newYPos}. Snap State = {_snapState} _initialPos.Y = {_initialPos.Y}. MaxY = {MaxY}.");
+            }
+        }
+    }
 }
--- Controls/UpgradableSection.cs
+++ Controls/UpgradableSection.cs
@@ -5,229 +5,229 @@ using Godot;
 
 public partial class UpgradableSection : Node2D
 {
-	// 100ms = 10 iterations in 1 second
-	// 10ms = 100 iterations in 1 second
-	// 1ms = 1000 iterations in 1 second
-	private const int CycleDelay = 5;
+    // 100ms = 10 iterations in 1 second
+    // 10ms = 100 iterations in 1 second
+    // 1ms = 1000 iterations in 1 second
+    private const int CycleDelay = 5;
 
-	private Button _upgradeButton;
-	private Sprite2D[] _upgradeIcons;
-	private ProgressBar _seedProgressBar;
-	private TextureProgressBar _seedProgressBar2;
-	private Label _itemNameLabel;
-	private Label _itemRateLabel;
-	private Label _seedCostLabel;
-	private Panel _maxedOutPanel;
-	private WorldScene _world;
-	private Seed _seed;
-	private int _multiplier = 1;
-	private Task _progressJob;
-	bool _isUpgradeButtonDown = false;
-	DateTime? _upgradeButtonDownTime = null;
+    private Button _upgradeButton;
+    private Sprite2D[] _upgradeIcons;
+    private ProgressBar _seedProgressBar;
+    private TextureProgressBar _seedProgressBar2;
+    private Label _itemNameLabel;
+    private Label _itemRateLabel;
+    private Label _seedCostLabel;
+    private Panel _maxedOutPanel;
+    private WorldScene _world;
+    private Seed _seed;
+    private int _multiplier = 1;
+    private Task _progressJob;
+    bool _isUpgradeButtonDown = false;
+    DateTime? _upgradeButtonDownTime = null;
 
-	private readonly TimeSpan OneSecondTimeSpan = TimeSpan.FromSeconds(1);
-	private readonly double MultiplierProgressionFactor = 10d;
+    private readonly TimeSpan OneSecondTimeSpan = TimeSpan.FromSeconds(1);
+    private readonly double MultiplierProgressionFactor = 10d;
 
-	private double MultiplierProgressionRate
-		=> Multiplier / MultiplierProgressionFactor;
+    private double MultiplierProgressionRate
+        => Multiplier / MultiplierProgressionFactor;
 
-	private double MaxMultiplier
-		=> MultiplierProgressionFactor * _seedProgressBar.MaxValue;
+    private double MaxMultiplier
+        => MultiplierProgressionFactor * _seedProgressBar.MaxValue;
 
-	private double ProgressBarCutOffMultiplier
-		=> MaxMultiplier * .5;
+    private double ProgressBarCutOffMultiplier
+        => MaxMultiplier * .5;
 
-	private bool IsMultiplierOverCutOff
-		=> Multiplier >= ProgressBarCutOffMultiplier;
+    private bool IsMultiplierOverCutOff
+        => Multiplier >= ProgressBarCutOffMultiplier;
 
-	public Seed SeedType
-	{
-		get => _seed;
+    public Seed SeedType
+    {
+        get => _seed;
 
-		set
-		{
-			if (_seed == value)
-			{
-				return;
-			}
+        set
+        {
+            if (_seed == value)
+            {
+                return;
+            }
 
-			_seed = value;
-			NotifyStateChanged();
-		}
-	}
-	public int Multiplier
-	{
-		get => _multiplier;
-		set
-		{
-			if (_multiplier == value)
-			{
-				return;
-			}
+            _seed = value;
+            NotifyStateChanged();
+        }
+    }
+    public int Multiplier
+    {
+        get => _multiplier;
+        set
+        {
+            if (_multiplier == value)
+            {
+                return;
+            }
 
-			_multiplier = value;
-			NotifyStateChanged();
-		}
-	}
+            _multiplier = value;
+            NotifyStateChanged();
+        }
+    }
 
-	public int Rate
-		=> (int)SeedType * Multiplier;
+    public int Rate
+        => (int)SeedType * Multiplier;
 
-	public const string ResourcePath = "res://Controls/upgradable_section.tscn";
+    public const string ResourcePath = "res://Controls/upgradable_section.tscn";
 
-	public EventHandler<long> PointCreated { get; set; }
-	public int UpgradeCost =>
-		(int)SeedType * (int)(SeedCosts.Multiplier / 10d) * Multiplier;
+    public EventHandler<long> PointCreated { get; set; }
+    public int UpgradeCost =>
+        (int)SeedType * (int)(SeedCosts.Multiplier / 10d) * Multiplier;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		var upgradePanel = GetNode<PanelContainer>("UpgradePanel");
-		_upgradeButton = upgradePanel.GetNode<Button>("UpgradeButton");
-		_upgradeButton.Pressed += Upgrade;
-		_upgradeButton.ButtonDown += () =>
-		{
-			_isUpgradeButtonDown = true;
-			_upgradeButtonDownTime = DateTime.Now;
-		};
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        var upgradePanel = GetNode<PanelContainer>("UpgradePanel");
+        _upgradeButton = upgradePanel.GetNode<Button>("UpgradeButton");
+        _upgradeButton.Pressed += Upgrade;
+        _upgradeButton.ButtonDown += () =>
+        {
+            _isUpgradeButtonDown = true;
+            _upgradeButtonDownTime = DateTime.Now;
+        };
 
-		_upgradeButton.ButtonUp += () =>
-		{
-			_isUpgradeButtonDown = false;
-			_upgradeButtonDownTime = null;
-		};
-
-
-		_upgradeIcons = new[]
-		{
-			upgradePanel.GetNode<Sprite2D>("UpgradeIcon"),
-			upgradePanel.GetNode<Sprite2D>("UpgradeIcon2")
-		};
-
-		SetUpgradeIconVisibility(visible: false);
-
-		_seedProgressBar = GetNode<ProgressBar>("SeedProgressBar");
-		_seedProgressBar2 = GetNode<TextureProgressBar>("SeedProgressBar2");
-		_itemNameLabel = GetNode<Label>("ItemNameLabel");
-		_itemRateLabel = GetNode<Label>("ItemRateLabel");
-		_seedCostLabel = GetNode<Panel>("SeedCostPanel").GetNode<Label>("SeedCostLabel");
-
-		_maxedOutPanel = GetNode<Panel>("MaxedOutPanel");
-
-		_world = GetParent().GetParent().GetParent().GetParent().GetNode<WorldScene>("WorldScene");
-
-		_progressJob ??= Task.Run(ProgressCycle);
-
-		NotifyStateChanged();
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if (_isUpgradeButtonDown && _upgradeButtonDownTime.HasValue)
-		{
-			var timeHeld = (DateTime.Now - _upgradeButtonDownTime.Value);
-			if (timeHeld > OneSecondTimeSpan)
-			{
-				Upgrade();
-			}
-		}
-
-		if (_seedProgressBar.Value == 100d)
-		{
-			if (Multiplier >= MaxMultiplier && !_maxedOutPanel.Visible)
-			{
-				_seedProgressBar.Visible = false;
-				_seedProgressBar2.Visible = false;
-
-				_maxedOutPanel.Visible = true;
-			}
-
-			_seedProgressBar2.Value = 0d;
-			_seedProgressBar.Value = 0d;
-			PointCreated?.Invoke(this, (int)SeedType);
-		}
-
-		SetUpgradeIconVisibility(CanPurchaseMultiplier());
-	}
+        _upgradeButton.ButtonUp += () =>
+        {
+            _isUpgradeButtonDown = false;
+            _upgradeButtonDownTime = null;
+        };
 
 
-	private void SetUpgradeIconVisibility(bool visible)
-	{
-		foreach (var upgradeIcon in _upgradeIcons)
-		{
-			upgradeIcon.Visible = visible;
+        _upgradeIcons = new[]
+        {
+            upgradePanel.GetNode<Sprite2D>("UpgradeIcon"),
+            upgradePanel.GetNode<Sprite2D>("UpgradeIcon2")
+        };
 
-		}
-	}
+        SetUpgradeIconVisibility(visible: false);
 
-	private async Task ProgressCycle()
-	{
-		GD.Print($"Progress bar min value: {_seedProgressBar.MinValue}, Max Value: {_seedProgressBar.MaxValue}");
+        _seedProgressBar = GetNode<ProgressBar>("SeedProgressBar");
+        _seedProgressBar2 = GetNode<TextureProgressBar>("SeedProgressBar2");
+        _itemNameLabel = GetNode<Label>("ItemNameLabel");
+        _itemRateLabel = GetNode<Label>("ItemRateLabel");
+        _seedCostLabel = GetNode<Panel>("SeedCostPanel").GetNode<Label>("SeedCostLabel");
 
-		while (true)
-		{
-			var progress = _seedProgressBar.Value;
+        _maxedOutPanel = GetNode<Panel>("MaxedOutPanel");
 
-			// Ensure the progress value is within the valid range
-			var newProgress = Mathf.Clamp(progress + MultiplierProgressionRate,
-				_seedProgressBar.MinValue,
-				_seedProgressBar.MaxValue);
+        _world = (WorldScene)GetTree().CurrentScene;
 
-			CallThreadSafe(nameof(SetProgressBarValue), newProgress);
+        _progressJob ??= Task.Run(ProgressCycle);
 
-			await Task.Delay(CycleDelay);
-		}
-	}
+        NotifyStateChanged();
+    }
 
-	private void SetProgressBarValue(double value)
-	{
-		_seedProgressBar.Value = value;
-		_seedProgressBar2.Value = value;
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (_isUpgradeButtonDown && _upgradeButtonDownTime.HasValue)
+        {
+            var timeHeld = (DateTime.Now - _upgradeButtonDownTime.Value);
+            if (timeHeld > OneSecondTimeSpan)
+            {
+                Upgrade();
+            }
+        }
 
-	private void BuyMultiplier()
-	{
-		_world.Points -= UpgradeCost;
-		Multiplier++;
-		NotifyStateChanged();
-	}
+        if (_seedProgressBar.Value == 100d)
+        {
+            if (Multiplier >= MaxMultiplier && !_maxedOutPanel.Visible)
+            {
+                _seedProgressBar.Visible = false;
+                _seedProgressBar2.Visible = false;
 
-	private void Upgrade()
-	{
-		if (!CanPurchaseMultiplier())
-		{
-			GD.Print($"Cannot afford to purchase upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
-			return;
-		}
+                _maxedOutPanel.Visible = true;
+            }
 
-		GD.Print($"Purchasing upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
-		BuyMultiplier();
-	}
+            _seedProgressBar2.Value = 0d;
+            _seedProgressBar.Value = 0d;
+            PointCreated?.Invoke(this, (int)SeedType);
+        }
 
-	private bool CanPurchaseMultiplier() =>
-		_world.Points >= UpgradeCost && Multiplier < MaxMultiplier;
+        SetUpgradeIconVisibility(CanPurchaseMultiplier());
+    }
 
-	private void NotifyStateChanged()
-	{
-		if (_itemNameLabel is not null)
-		{
-			_itemNameLabel.Text = $"{SeedType}";
-		}
 
-		if (_itemRateLabel is not null)
-		{
-			_itemRateLabel.Text = $"{((long)SeedType).FormatLargeNumber()}";
-		}
+    private void SetUpgradeIconVisibility(bool visible)
+    {
+        foreach (var upgradeIcon in _upgradeIcons)
+        {
+            upgradeIcon.Visible = visible;
 
-		if (_upgradeButton is not null)
-		{
-			_upgradeButton.Text = $"x{((long)Multiplier).FormatLargeNumber()}";
-		}
+        }
+    }
 
-		if (_seedCostLabel is not null)
-		{
-			_seedCostLabel.Text = $"{((long)UpgradeCost).FormatLargeNumber()}";
-		}
-	}
+    private async Task ProgressCycle()
+    {
+        GD.Print($"Progress bar min value: {_seedProgressBar.MinValue}, Max Value: {_seedProgressBar.MaxValue}");
+
+        while (true)
+        {
+            var progress = _seedProgressBar.Value;
+
+            // Ensure the progress value is within the valid range
+            var newProgress = Mathf.Clamp(progress + MultiplierProgressionRate,
+                _seedProgressBar.MinValue,
+                _seedProgressBar.MaxValue);
+
+            CallThreadSafe(nameof(SetProgressBarValue), newProgress);
+
+            await Task.Delay(CycleDelay);
+        }
+    }
+
+    private void SetProgressBarValue(double value)
+    {
+        _seedProgressBar.Value = value;
+        _seedProgressBar2.Value = value;
+    }
+
+    private void BuyMultiplier()
+    {
+        _world.Points -= UpgradeCost;
+        Multiplier++;
+        NotifyStateChanged();
+    }
+
+    private void Upgrade()
+    {
+        if (!CanPurchaseMultiplier())
+        {
+            GD.Print($"Cannot afford to purchase upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
+            return;
+        }
+
+        GD.Print($"Purchasing upgrade for {SeedType}. {_world.Points} < {UpgradeCost}!");
+        BuyMultiplier();
+    }
+
+    private bool CanPurchaseMultiplier() =>
+        _world.Points >= UpgradeCost && Multiplier < MaxMultiplier;
+
+    private void NotifyStateChanged()
+    {
+        if (_itemNameLabel is not null)
+        {
+            _itemNameLabel.Text = $"{SeedType}";
+        }
+
+        if (_itemRateLabel is not null)
+        {
+            _itemRateLabel.Text = $"{((long)SeedType).FormatLargeNumber()}";
+        }
+
+        if (_upgradeButton is not null)
+        {
+            _upgradeButton.Text = $"x{((long)Multiplier).FormatLargeNumber()}";
+        }
+
+        if (_seedCostLabel is not null)
+        {
+            _seedCostLabel.Text = $"{((long)UpgradeCost).FormatLargeNumber()}";
+        }
+    }
 }
